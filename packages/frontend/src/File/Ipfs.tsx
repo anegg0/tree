94ccabe167219e6a8e9d0f4bee1fb32d8c75176e
anegg0 <gael.blanchemain@protonmail.com>
2020-09-01 13:55:40 -0700

    linted ts errors

---
+++ packages/frontend/src/File/Ipfs.tsx
@@ -0,0 +1,14 @@
+class IpfsFile {
+  hash?: string;
+  time?: string;
+  exists?: boolean;
+  url?: string;
+  constructor(hash?: string, time?: string, exists?: boolean, url?: string) {
+    this.hash = hash;
+    this.time = time;
+    this.exists = exists;
+    this.url = url;
+  }
+}
+
+export { IpfsFile };
--- packages/frontend/src/types.ts
+++ packages/frontend/src/types.ts
@@ -1,3 +1,11 @@
 export interface Auth {
   accessToken: string;
 }
+
+
+export interface IpfsFile {
+  hash?: string;
+  time?: string;
+  exists?: boolean;
+  url?: string;
+}
