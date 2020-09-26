bf841372e4b6f7cbc82c8ac1b0d82acff3ac8698
anegg0 <gael.blanchemain@protonmail.com>
2020-09-26 15:05:58 -0700

    functional but ugly

--- packages/frontend/src/App/App.tsx
+++ packages/frontend/src/App/App.tsx
@@ -1,4 +1,3 @@
-import './App.css';
 
 import FileUpLoader from './FileUpLoader';
 import React from 'react';
@@ -9,87 +8,86 @@ import { Login } from '../Login';
 import { Profile } from '../Profile/Profile';
 import { Auth } from '../types';
 import logo from './logo.svg';
-
-const LS_KEY = 'login-with-metamask:auth';
-
-interface State {
-    webServiceErrorStatus: string;
-    auth?: Auth;
-    isUploadLoading: boolean;
-    isSearchLoading: boolean;
-    isReaderError: boolean;
-    isAfterUpload: boolean;
-    isAfterSearch: boolean;
-    fileHash: string;
-    fileInfo: IpfsFile;
-    accessToken: string;
-    uploadedHashes: string[];
+const LS_KEY = 'login-with-metamask:auth'; interface State {
+  webServiceErrorStatus: string; auth?: Auth;
+  isUploadLoading: boolean;
+  isSearchLoading: boolean;
+  isReaderError: boolean;
+  isAfterUpload: boolean;
+  isAfterSearch: boolean;
+  fileHash: string;
+  fileInfo: IpfsFile;
+  accessToken: string;
+  uploadedHashes: string[];
 }
 
 interface Props {
-    auth: Auth;
-    onLoggedOut: () => void;
+  auth: Auth;
+  onLoggedOut: () => void;
 }
 export class App extends React.Component<{}, State> {
-    state: State = {
-        accessToken: '',
-        webServiceErrorStatus: '',
-        isUploadLoading: false,
-        isSearchLoading: false,
-        isReaderError: false,
-        isAfterUpload: false,
-        isAfterSearch: false,
-        fileHash: '',
-        fileInfo: {
-            hash: '',
-            time: '',
-            exists: false,
-            url: '',
-        },
-        uploadedHashes: [''],
-        auth: this.state,
-    };
+  state: State = {
+    accessToken: '',
+    webServiceErrorStatus: '',
+    isUploadLoading: false,
+    isSearchLoading: false,
+    isReaderError: false,
+    isAfterUpload: false,
+    isAfterSearch: false,
+    fileHash: '',
+    fileInfo: {
+      hash: '',
+      time: '',
+      exists: false,
+      url: '',
+    },
+    uploadedHashes: [''],
+    auth: this.state,
+  };
 
-    componentDidMount(): void {
-        // Access token is stored in localstorage
-        const ls = window.localStorage.getItem(LS_KEY);
-        const auth = ls && JSON.parse(ls);
-        const fileInfo = new IpfsFile(); // this.setState(fileInfo);
-        this.setState({ auth, fileInfo });
-    }
+  componentDidMount(): void {
+    // Access token is stored in localstorage
+    const ls = window.localStorage.getItem(LS_KEY);
+    const auth = ls && JSON.parse(ls);
+    const fileInfo = new IpfsFile(); // this.setState(fileInfo);
+    this.setState({ auth, fileInfo });
+  }
 
-    handleLoggedIn = (auth: Auth): void => {
-        localStorage.setItem(LS_KEY, JSON.stringify(auth));
-        this.setState({ auth });
-    };
+  handleLoggedIn = (auth: Auth): void => {
+    localStorage.setItem(LS_KEY, JSON.stringify(auth));
+    this.setState({ auth });
+  };
 
-    handleLoggedOut = (): void => {
-        localStorage.removeItem(LS_KEY);
-        this.setState({ auth: undefined });
-    };
+  handleLoggedOut = (): void => {
+    localStorage.removeItem(LS_KEY);
+    this.setState({ auth: undefined });
+  };
 
-    render(): JSX.Element {
-        const { auth } = this.state;
-        return (
-            <Segment basic className="App">
-                <h1 className="App-title">Welcome to Tree Proof of Plant</h1>
-                <div className="App-intro">
-                    {auth ? (
-                        <div>
-                            <Profile auth={auth} onLoggedOut={this.handleLoggedOut} />
-                            <div>
-                                <FileUpLoader />
-                            </div>
-                        </div>
-                    ) : (
-                            <div>
-                                <Login onLoggedIn={this.handleLoggedIn} />
-                            </div>
-                        )}
+  render(): JSX.Element {
+    const { auth } = this.state;
+    return (
+      <>
+        <Grid.Row columns={3}>
+          <Segment basic className="App">
+            <h1 className="App-title">Welcome to Tree Proof of Plant</h1>
+            <div className="App-intro"></div>
+            {auth ? (
+              <div>
+                <Profile auth={auth} onLoggedOut={this.handleLoggedOut} />
+                <div>
+                  <FileUpLoader />
                 </div>
-            </Segment>
-        );
-    }
-}
+              </div>
+            ) : (
+                <div>
+                  <Login onLoggedIn={this.handleLoggedIn} />
+                </div>
+              )}
+          </Segment>
+        </Grid.Row>
+    );
+        {/* `{'}'}` */}
 
-export default App;
+      </>
+    );
+}}
--- packages/frontend/src/App/FileGetter.tsx
+++ packages/frontend/src/App/FileGetter.tsx
@@ -1,76 +1,85 @@
-import React, { useState } from 'react';
-import { Grid, Card, Segment, Form, Input, Button, Message } from 'semantic-ui-react';
-import { getImageIpfsHash } from './getIpfsHash';
 import 'semantic-ui-css/semantic.min.css'
 
+import React, { useState } from 'react';
+import {
+  Button,
+  Card,
+  Form,
+  Grid,
+  Input,
+  Message,
+  Segment,
+} from 'semantic-ui-react';
+
+import { getImageIpfsHash } from './getIpfsHash';
+
 const FileGetter = () => {
-    const [buffer, setBuffer] = useState<ArrayBuffer>(new ArrayBuffer(0));
-    const [resultHash, setResultHash] = useState('');
-    const [load, setLoad] = useState<boolean>(true);
-    const [end, setEnd] = useState(false);
+  const [buffer, setBuffer] = useState<ArrayBuffer>(new ArrayBuffer(0));
+  const [resultHash, setResultHash] = useState('');
+  const [load, setLoad] = useState<boolean>(true);
+  const [end, setEnd] = useState(false);
 
-    const handleChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
-        const files = e.target.files as FileList;
-        const reader = new window.FileReader();
-        reader.readAsArrayBuffer(files[0]);
-        reader.onloadend = () => {
-            const res = reader.result as string;
-            setBuffer(Buffer.from(res));
-            console.log(reader.result);
-        };
-        console.log(buffer);
+  const handleChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    const files = e.target.files as FileList;
+    const reader = new window.FileReader();
+    reader.readAsArrayBuffer(files[0]);
+    reader.onloadend = () => {
+      const res = reader.result as string;
+      setBuffer(Buffer.from(res));
+      console.log(reader.result);
     };
+    console.log(buffer);
+  };
 
-    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        console.log('submitted...');
-        setLoad(false);
-        const hash: string = await getImageIpfsHash(buffer);
-        setResultHash(hash);
-        console.log(hash);
-        setEnd(true);
-    };
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log('submitted...');
+    setLoad(false);
+    const hash: string = await getImageIpfsHash(buffer);
+    setResultHash(hash);
+    console.log(hash);
+    setEnd(true);
+  };
 
-    return (
-        <>
-            <Grid divided='vertically'>
-                <Grid.Row columns={1}>
-
-                    <Grid.Column>
-                        <Segment.Group>
-                            <Segment>
-                                <Card.Header> File Uploader</Card.Header>
-                                <Card.Meta>Max size: 5m</Card.Meta>
-                                <Form onSubmit={handleSubmit}>
-                                    <Card.Description>
-                                        Select file, then hit "Submit"
-                      </Card.Description>
-                                    <Form.Field>
-                                        <Input name="username" type="file" onChange={handleChange}></Input>
-                                    </Form.Field>
-                                    <Button>Submit</Button>
-                                </Form>
-                            </Segment>
-                            {load ? <></> : <Message as="h3">Uploading...</Message>}
-                            {end ? <Message positive>Upload successful </Message> : <></>}
-                            <Segment>IPFS Hash : {resultHash}</Segment>
-                            <Segment>
-                                Check your IPFS Link {' '}
-                                <a
-                                    href={`https://ipfs.io/ipfs/${resultHash}`}
-                                    target="_blank"
-                                    rel="noreferrer noopener"
-                                >
-                                    here
+  return (
+    <>
+      <Grid divided="vertically">
+        <Grid.Row columns={1}>
+          <Grid.Column>
+            <Segment.Group>
+              <Segment>
+                <Card.Header> File Uploader</Card.Header>
+                <Card.Meta>Max size: 5m</Card.Meta>
+                <Form onSubmit={handleSubmit}>
+                  <Card.Description>
+                    Select file, then hit "Submit"
+  </Card.Description>
+                  <Form.Field>
+                    <Input name="username" type="file" onChange={handleChange}></Input>
+                  </Form.Field>
+                  <Button>Submit</Button>
+                </Form>
+              </Segment>
+              {load ? <></> : <Message as="h3">Uploading...</Message>}
+              {end ? <Message positive>Upload successful </Message> : <></>}
+              <Segment>IPFS Hash : {resultHash}</Segment>
+              <Segment>
+                Check your IPFS Link {' '}
+                <a
+                  href={`https://ipfs.io/ipfs/${resultHash}`}
+                  target="_blank"
+                  rel="noreferrer noopener"
+                >
+                  here
           </a>
-                            </Segment>
-                        </Segment.Group>
-                    </Grid.Column>
-                </Grid.Row>
-            </Grid>
+              </Segment>
+            </Segment.Group>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
 
-        </>
-    );
+    </>
+  );
 };
 
 export default FileGetter;
